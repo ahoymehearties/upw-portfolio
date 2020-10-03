ded5303b49bc52d57ea93a7f84b6d491e6deb4bf
edindzabija <edindzabija@gmail.com>
2020-10-03 22:05:41 +0200

    writing on index

--- src/templates/voice.js
+++ src/templates/voice.js
@@ -32,22 +32,21 @@ const Voice = props => {
     <Layout>
       <Head title={props.data.contentfulVoice.title} />
       {/* header */}
-      <div className="text-center">
+      <div className="text-center mt-12">
         <h1 className="font-bold break-normal text-3xl md:text-5xl">
           {props.data.contentfulVoice.title}
         </h1>
       </div>
-      <div class="mx-0 sm:mx-6">
-        {/* header */}
 
-        <div
-          className=" w-full p-8 md:p-24 md:pt-2 text-xl md:text-2xl text-grey-darkest leading-normal"
-          style={{ fontFamily: `Georgia,serif` }}
-          dangerouslySetInnerHTML={{
-            __html: props.data.contentfulVoice.body.childMarkdownRemark.html,
-          }}
-        ></div>
-      </div>
+      {/* header */}
+
+      <div
+        className="w-full p-8 md:p-24 md:pt-2 text-xl md:text-2xl text-grey-darkest leading-normal"
+        style={{ fontFamily: `Georgia,serif` }}
+        dangerouslySetInnerHTML={{
+          __html: props.data.contentfulVoice.body.childMarkdownRemark.html,
+        }}
+      ></div>
     </Layout>
   )
 }
