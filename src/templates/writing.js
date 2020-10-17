500bcd4a4c856b941c67344d828eae6afdd4c107
edindzabija <edindzabija@gmail.com>
2020-10-17 19:55:58 +0200

    layout change writing template

--- src/templates/writing.js
+++ src/templates/writing.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Link from "gatsby-plugin-transition-link"
 
 import Head from "../components/Head"
 
@@ -7,6 +8,10 @@ export const query = graphql`
   query($slug: String!) {
     contentfulWriting(slug: { eq: $slug }) {
       title
+      position
+      year
+      developer
+      website
       headerImage {
         fluid {
           src
@@ -30,20 +35,51 @@ export const query = graphql`
 const Writing = props => {
   return (
     <>
-      <Head
-        title={props.data.contentfulWriting.title}
-        description={
-          props.data.contentfulWriting.body.childMarkdownRemark.excerpt
-        }
-      />
-      {/* header */}
-      <div className="text-center mt-12">
-        <h1 className="font-bold break-normal text-3xl md:text-5xl">
-          {props.data.contentfulWriting.title}
-        </h1>
+      <Head title={props.data.contentfulWriting.title} />
+
+      <div className="md:flex shadow-lg mx-6 md:mx-auto max-w-screen-lg my-10 h-auto">
+        <img
+          className="h-full w-full md:w-1/2 object-cover md:rounded-lg rounded-r-none pb-5/6"
+          src={props.data.contentfulWriting.headerImage.fluid.src}
+          alt={props.data.contentfulWriting.title}
+        />
+        <div className="w-full md:w-1/2 md:pl-10 p-4 pt-2 bg-orange-100 rounded-lg flex items-center">
+          <div>
+            <div className="pb-4">
+              {/* <span className="text-gray-800 text-3xl font-semibold">Title: </span> */}
+              <h2 className="text-gray-800 text-4xl font-semibold uppercase underline">
+                {props.data.contentfulWriting.title}
+              </h2>
+            </div>
+            <div className="text-xl pb-2">
+              <span className="font-semibold">Position: </span>
+              <span>{props.data.contentfulWriting.position}</span>
+            </div>
+            {/* <div className="text-xl pb-2">
+              <span className="font-semibold">Vocal Role(s): </span>
+              {props.data.contentfulWriting.vocalRole}
+            </div> */}
+            <div className="text-xl pb-2">
+              <span className="font-semibold">Year: </span>
+              {props.data.contentfulWriting.year}
+            </div>
+            <div className="text-xl pb-2">
+              <span className="font-semibold">Developer: </span>
+              {props.data.contentfulWriting.developer}
+            </div>
+            <div className="text-xl">
+              <span className="font-semibold">Website: </span>
+              <span className="underline italic font-semibold text-orange-400 hover:text-orange-600">
+                <Link to={props.data.contentfulWriting.website}>
+                  {" "}
+                  Official Website{" "}
+                </Link>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="mx-0 sm:mx-6">
-        {/* header */}
+      {props.data.contentfulWriting.body && (
         <div
           className="mx-6 md:mx-auto max-w-screen-lg my-10"
           style={{ fontFamily: `Georgia,serif` }}
@@ -51,7 +87,7 @@ const Writing = props => {
             __html: props.data.contentfulWriting.body.childMarkdownRemark.html,
           }}
         ></div>
-      </div>
+      )}
     </>
   )
 }
